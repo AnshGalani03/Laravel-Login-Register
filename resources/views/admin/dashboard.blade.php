dc4e0c8a2697e704e55e4243b96c13a5b4f54c91
AnshGalani03 <anshgalani@yahoo.com>
2024-06-21 12:07:29 +0530

    Remove White Space

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,7 +15,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
             @endif
-            <div class="card mt-4">
+            <div class="card mt-4"> 
                 <div class="card-body">
                     <div class="product-table">
                         <div class="table-responsive">
